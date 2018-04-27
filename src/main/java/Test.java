003c6605781f9e30096b6606b761b8bf69187e35
ankang66 <ankang2211@126.com>
2018-04-27 21:09:12 +0800

    我第二次去参加

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -2,6 +2,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("我参加");
         System.out.println("我第一次参加");
-
+        System.out.println("我第二次参加");
     }
 }
